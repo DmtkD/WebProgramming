3a10afc328d46ad49b0807333cee587408ed4392
pavlo <pavlo.to474@gmail.com>
2022-11-03 04:21:51 +0200

    some fix? and i go sleep

--- Lab 8 — React.js Item page/src/components/Filter/Filter.jsx
+++ Lab 8 — React.js Item page/src/components/Filter/Filter.jsx
@@ -10,10 +10,12 @@ function Filter({ sendDataToParent }) {
     const filterCard = async (event) => {
         event.preventDefault();
         let findCard = [432423];
+        let left = parseInt(memoryValue.toString().split(",")[0]);
+        let right = parseInt(memoryValue.toString().split(",")[1]);
         if (memoryValue === "Memory" && companyValue === "Company") { findCard = cardItem }
-        if (memoryValue !== "Memory" && companyValue === "Company") { findCard = cardItem.filter((el) => el.memoryCapacity <= memoryValue) }
+        if (memoryValue !== "Memory" && companyValue === "Company") { findCard = cardItem.filter((el) => el.memoryCapacity >= left && el.memoryCapacity <= right) }
         if (memoryValue === "Memory" && companyValue !== "Company") { findCard = cardItem.filter((el) => el.nameOfManufacturer === companyValue) }
-        if (memoryValue !== "Memory" && companyValue !== "Company") { findCard = cardItem.filter((el) => el.memoryCapacity <= memoryValue && el.nameOfManufacturer === companyValue) }
+        if (memoryValue !== "Memory" && companyValue !== "Company") { findCard = cardItem.filter((el) => el.memoryCapacity >= left && el.memoryCapacity <= right && el.nameOfManufacturer === companyValue) }
         sendDataToParent(findCard);
     }
 
@@ -22,11 +24,11 @@ function Filter({ sendDataToParent }) {
             <form>
                 <div>
                     <select onChange={(event) => { setMemoryValue(event.target.value) }}>
-                        <option value={"Memory"}>Memory</option>
-                        <option value={32}>To 32 Gb</option>
-                        <option value={64}>From 32 to 64</option>
-                        <option value={128}>From 64 to 128</option>
-                        <option value={"2048"}>From 128 Gb...</option>
+                        <option multiple={true} value={"Memory"}>Memory</option>
+                        <option multiple={true} value={["0", "32"]}>To 32 Gb</option>
+                        <option multiple={true} value={["32", "64"]}>From 32 to 64</option>
+                        <option multiple={true} value={["64", "128"]}>From 64 to 128</option>
+                        <option multiple={true} value={["128", "2048"]}>From 128 Gb...</option>
                     </select>
                     <select onChange={(event) => { setCompanyValue(event.target.value) }}>
                         <option value={"Company"}>Company</option>
